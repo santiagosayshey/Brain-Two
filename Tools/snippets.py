69c0fca36176642c50a59db6f54f7823246b0b12
santiagosayshey <chaucted@gmail.com>
2023-08-16 11:16:01 +0930

    vault backup: 2023-08-16 11:16:01

--- Tools/snippets.py
+++ Tools/snippets.py
@@ -1,34 +1,56 @@
 import os
 import re
 
-def extract_code_blocks(filepath):
-    """Extract code blocks from a markdown file."""
-    with open(filepath, 'r', encoding='utf-8') as f:
-        content = f.read()
+def correct_extension(ext):
+    """
+    Correct the file extension based on the mapping.
+    """
+    # Mapping of incorrect extensions to the correct ones
+    ext_mapping = {
+        '.bash': '.sh',
+        '.console': '.sh',
+        '.hdl': '.v', # Verilog file extension
+        '.java': '.js', # Changing to JavaScript as per your request
+        '.javascript': '.js',
+        '.python': '.py',
+        '.sql': '.sql', # Keeping it as .sql as it is recognized by GitHub
+        '.pseudocode': '.cpp'
+    }
+    # Return the corrected extension, or the original one if it's not in the mapping
+    return ext_mapping.get(ext, ext)
 
-    # Regular expression pattern for finding code blocks
-    pattern = re.compile(r'```(?P<language>\w+)?\n(?P<code>.*?)```', re.S)
-    return pattern.findall(content)
-
-def save_code_blocks_to_files(code_blocks, output_dir):
-    """Save each code block to a separate file with the proper extension."""
-    for idx, (language, code) in enumerate(code_blocks):
-        # Default to .txt if no language is specified
-        ext = language if language else 'txt'
-        filename = f'code_block_{idx + 1}.{ext}'
-        output_path = os.path.join(output_dir, filename)
-        
-        with open(output_path, 'w', encoding='utf-8') as f:
-            f.write(code)
-
-def process_markdown_files(root_dir, output_dir):
-    """Process all markdown files in a directory and its subdirectories."""
-    for root, _, files in os.walk(root_dir):
+def save_code_blocks_to_files(src_dir, dest_dir):
+    """
+    Extract code blocks from Markdown files in the source directory,
+    and save them to files with proper extensions in the destination directory.
+    """
+    # Regular expression to match code blocks in Markdown
+    code_block_pattern = re.compile(r'```(.+?)\n(.*?)```', re.S)
+    
+    # Iterate through the source directory and its subdirectories
+    for subdir, _, files in os.walk(src_dir):
         for file in files:
             if file.endswith('.md'):
-                filepath = os.path.join(root, file)
-                code_blocks = extract_code_blocks(filepath)
-                save_code_blocks_to_files(code_blocks, output_dir)
+                # Open and read the content of the Markdown file
+                with open(os.path.join(subdir, file), 'r', encoding='utf8') as md_file:
+                    content = md_file.read()
+                
+                # Find all code blocks in the content
+                code_blocks = code_block_pattern.findall(content)
+                
+                # Process each code block
+                for i, (lang, code) in enumerate(code_blocks):
+                    # Correct the file extension
+                    lang = correct_extension('.' + lang.strip().lower())
+                    
+                    # Generate the output file name
+                    output_filename = f"{file[:-3]}_{i + 1}{lang}"
+                    
+                    # Write the code block to the corresponding file
+                    output_path = os.path.join(dest_dir, output_filename)
+                    with open(output_path, 'w', encoding='utf8') as out_file:
+                        out_file.write(code.strip())
+
 
 # Example usage
 if __name__ == "__main__":
