f89fbab2f5d9dd7502397d108a0b66a47512fc28
santiagosayshey <chaucted@gmail.com>
2023-08-16 11:06:02 +0930

    vault backup: 2023-08-16 11:06:02

--- Tools/snippets.py
+++ Tools/snippets.py
@@ -3,7 +3,7 @@ import re
 
 def extract_code_blocks(filepath):
     """Extract code blocks from a markdown file."""
-    with open(filepath, 'r') as f:
+    with open(filepath, 'r', encoding='utf-8') as f:
         content = f.read()
 
     # Regular expression pattern for finding code blocks
@@ -18,19 +18,25 @@ def save_code_blocks_to_files(code_blocks, output_dir):
         filename = f'code_block_{idx + 1}.{ext}'
         output_path = os.path.join(output_dir, filename)
         
-        with open(output_path, 'w') as f:
+        with open(output_path, 'w', encoding='utf-8') as f:
             f.write(code)
 
-def process_markdown_files(root_dir):
+def process_markdown_files(root_dir, output_dir):
     """Process all markdown files in a directory and its subdirectories."""
     for root, _, files in os.walk(root_dir):
         for file in files:
             if file.endswith('.md'):
                 filepath = os.path.join(root, file)
                 code_blocks = extract_code_blocks(filepath)
-                save_code_blocks_to_files(code_blocks, root)
+                save_code_blocks_to_files(code_blocks, output_dir)
 
 # Example usage
 if __name__ == "__main__":
-    root_directory = './markdown_files' # specify the root directory containing your markdown files here
-    process_markdown_files(root_directory)
+    root_directory = r'C:\path\to\your\markdown_files' # specify the root directory containing your markdown files here
+    output_directory = r'C:\Computer Science\Snippets' # specify the directory where you want to save the code snippets
+    
+    # Create the output directory if it does not exist
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+        
+    process_markdown_files(root_directory, output_directory)
